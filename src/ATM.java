7d9e1818dceb313b864cb453e67190f9b09d9a3c
Brandon Wong <brandonfn2007@gmail.com>
2024-01-23 01:14:04 -0500

    fixed ATM

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+//private instance variables
 public class ATM {
     private String userName;
     private int userPIN;
@@ -7,18 +8,30 @@ public class ATM {
     private Account savings;
     private Account checking;
     private Scanner scan;
+    private Customer customer;
+    private TransactionHistory TH;
+
 
     public void start() {
         accountInfo();
     }
+
+    //initializes the private instance variables
     public void accountInfo() {
         scan = new Scanner(System.in);
+        customer = new Customer(userName, userPIN);
+        savings = new Account("savings", customer);
+        checking = new Account("checking", customer);
+        TH = new TransactionHistory("");
+        scan = new Scanner(System.in);
 
+        // welcomes user
         System.out.println("Welcome banker to Aurum Fundamentals! We are delighted to have you as a new member!");
         System.out.print("What would you like the name for your account to be? ");
         userName = scan.nextLine();
         System.out.print("Welcome " + userName + ". Please enter a 4-digit PIN for your new account: ");
         int maybePIN = scan.nextInt();
+        scan.nextLine();
         if (String.valueOf(maybePIN).length() == 4) {
             userPIN = maybePIN;
         } else {
@@ -26,10 +39,10 @@ public class ATM {
             accountInfo();
         }
 
-        Customer customer = new Customer(userName, userPIN);
-
+        // confirms PIN
         System.out.print("Please re-enter your PIN: ");
         int PIN = scan.nextInt();
+        scan.nextLine();
         if (PIN == customer.getPIN()) {
             accountActions();
         } else {
@@ -38,12 +51,8 @@ public class ATM {
         }
     }
     public void accountActions() {
-        scan = new Scanner(System.in);
-        Customer customer = new Customer(userName, userPIN);
-        savings = new Account("savings", customer);
-        checking = new Account("checking", customer);
-        TransactionHistory TH = new TransactionHistory("");
 
+        // main menu
         System.out.println("1. Withdraw money\n" +
                 "2. Deposit money\n" +
                 "3. Transfer money between accounts\n" +
@@ -54,61 +63,77 @@ public class ATM {
 
         System.out.print("Enter a number: ");
         int choice = scan.nextInt();
+        scan.nextLine();
         if (choice == 1) {
+            // withdraws money from savings or checking account
             System.out.print("Which account would you like to withdraw money from? ");
             String account = scan.nextLine();
             if (account.equals("savings")) {
                 System.out.print("How much money would you like to withdraw? You can only withdraw $5s and $20s and do not include $ in your answer. ");
                 int withdraw = scan.nextInt();
+                scan.nextLine();
+                // makes sure there are sufficient funds and withdraw amount is a multiple of 5
                 if (withdraw <= savings.getCurrentBalance() && withdraw % 5 == 0) {
                     System.out.print("How many $5s would you like? ");
                     int numFives = scan.nextInt();
+                    scan.nextLine();
                     System.out.print("How many $20s would you like? ");
                     int num20s = scan.nextInt();
+                    scan.nextLine();
+                    // checks if the sum of the $5 and $20 bills add up to the withdrawal amount
                     if (numFives * 5 + num20s * 20 == withdraw) {
                         savings.setCurrentBalance(-withdraw);
                         transactionHistoryNum++;
                         System.out.println("A000" + transactionHistoryNum + ". Withdrew $" + withdraw + " from savings account");
+                        // edits transaction history
                         TH.setTransactionHistory("A000" + transactionHistoryNum + ". Withdrew $" + withdraw + " from savings account\n");
                     } else {
                         System.out.println("Unfortunately, your bills did not add up. \nPlease try again. ");
                         accountActions();
                     }
                 } else {
-                    System.out.println("Insufficient funds or withdraw amount invalid");
+                    System.out.println("Insufficient funds or invalid withdraw amount ");
                     accountActions();
                 }
             }
             if (account.equals("checking")) {
                 System.out.print("How much money would you like to withdraw? You can only withdraw $5s and $20s and do not include $ in your answer. ");
                 int withdraw = scan.nextInt();
+                scan.nextLine();
+                // makes sure there are sufficient funds and withdraw amount is a multiple of 5
                 if (withdraw <= checking.getCurrentBalance() && withdraw % 5 == 0) {
                     System.out.print("How many $5s would you like? ");
                     int numFives = scan.nextInt();
+                    scan.nextLine();
                     System.out.print("How many $20s would you like? ");
                     int num20s = scan.nextInt();
+                    scan.nextLine();
+                    // checks if the sum of the $5 and $20 bills add up to the withdrawal amount
                     if (numFives * 5 + num20s * 20 == withdraw) {
                         checking.setCurrentBalance(-withdraw);
                         transactionHistoryNum++;
                         System.out.println("A000" + transactionHistoryNum + ". Withdrew $" + withdraw + " from checking account");
+                        // edits transaction history
                         TH.setTransactionHistory("A000" + transactionHistoryNum + ". Withdrew $" + withdraw + " from checking account\n");
                     } else {
                         System.out.println("Unfortunately, your bills did not add up. \nPlease try again. ");
                         accountActions();
                     }
                 } else {
-                    System.out.println("Insufficient funds or withdraw amount invalid");
+                    System.out.println("Insufficient funds or invalid withdraw amount");
                     accountActions();
                 }
             }
             accountActions();
         }
         if (choice == 2) {
+            // deposits money into savings or checking account
             System.out.print("Which account would you like to deposit money into? ");
             String account = scan.nextLine();
             if (account.equals("savings")) {
                 System.out.print("How much money would you like to deposit? ");
                 double deposit = scan.nextDouble();
+                scan.nextLine();
                 savings.setCurrentBalance(deposit);
                 transactionHistoryNum++;
                 System.out.println("A000" + transactionHistoryNum + ". Deposited $" + deposit + " into savings account");
@@ -117,19 +142,24 @@ public class ATM {
             if (account.equals("checking")) {
                 System.out.print("How much money would you like to deposit? ");
                 double deposit = scan.nextDouble();
+                scan.nextLine();
                 checking.setCurrentBalance(deposit);
                 transactionHistoryNum++;
                 System.out.println("A000" + transactionHistoryNum + ". Deposited $" + deposit + " into checking account");
                 TH.setTransactionHistory("A000" + transactionHistoryNum + ". Deposited $" + deposit + " into checking account\n");
             }
+            // returns to main menu
             accountActions();
         }
         if (choice == 3) {
-            System.out.println("Which account would you like to transfer money to? ");
+            // transfers money between savings and checking accounts
+            System.out.print("Which account would you like to transfer money to? ");
             String transferTo = scan.nextLine();
             if (transferTo.equals("savings")) {
                 System.out.print("How much money would you like to transfer to savings? ");
                 double transfer = scan.nextDouble();
+                scan.nextLine();
+                // checks if there are sufficient funds in checking to transfer the amount to savings
                 if (transfer <= checking.getCurrentBalance()) {
                     savings.setCurrentBalance(transfer);
                     checking.setCurrentBalance(-transfer);
@@ -144,6 +174,8 @@ public class ATM {
             if (transferTo.equals("checking")) {
                 System.out.print("How much money would you like to transfer to checking? ");
                 double transfer = scan.nextDouble();
+                scan.nextLine();
+                // checks if there are sufficient funds in savings to transfer the amount to checking
                 if (transfer <= savings.getCurrentBalance()) {
                     checking.setCurrentBalance(transfer);
                     savings.setCurrentBalance(-transfer);
@@ -158,6 +190,7 @@ public class ATM {
             accountActions();
         }
         if (choice == 4) {
+            // prints current balances of savings and checking
             System.out.println("savings account balance: $" + savings.getCurrentBalance() + "\n" + "checking account balance: $" + checking.getCurrentBalance());
             transactionHistoryNum2++;
             System.out.println("S000" + transactionHistoryNum2 + ". Checked account balances of savings and checking accounts");
@@ -165,6 +198,7 @@ public class ATM {
             accountActions();
         }
         if (choice == 5) {
+            // prints transaction history
             System.out.println(TH.getTransactionHistory());
             transactionHistoryNum2++;
             System.out.println("S000" + transactionHistoryNum2 + ". Viewed transaction history");
@@ -172,11 +206,14 @@ public class ATM {
             accountActions();
         }
         if (choice == 6) {
+            // changes PIN
             System.out.print("What is your current PIN? ");
             int currentPIN = scan.nextInt();
+            scan.nextLine();
             if (currentPIN == customer.getPIN()) {
                 System.out.print("What would you like your new PIN to be? ");
                 int newPIN = scan.nextInt();
+                scan.nextLine();
                 customer.setPIN(newPIN);
                 transactionHistoryNum2++;
                 System.out.println("S000" + transactionHistoryNum2 + ". Changed PIN to " + newPIN);
@@ -187,14 +224,11 @@ public class ATM {
             accountActions();
         }
         if (choice == 7) {
-            System.out.println("Thank you for banking with Aurum Fundamentals! We await your return " + userName);
+            // ends ATM and user interaction
+            System.out.println("Thank you for banking with Aurum Fundamentals! We await your return " + userName + "!");
             System.exit(0);
         } else {
             accountActions();
         }
     }
 }
-
-
-
-
